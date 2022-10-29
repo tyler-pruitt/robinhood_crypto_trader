7ae8001a736be76ddbea91382ce79a95425bdabf
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2022-10-28 17:38:03 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -30,33 +30,34 @@ Join Robinhood with my link and we'll both pick our own free stock 🤝 https://
 import trader
 
 config = {
-            'crypto': ['BTC', 'ETH'],
-            'username': 'your_robinhood_username',
-            'password': 'your_robinhood_password',
-            'days_to_run': 1,
-            'export_csv': False,
-            'plot_analytics': False,
-            'plot_crypto': False,
-            'plot_portfolio': False,
-            'mode': 'safelive',
-            'backtest': {
-                'interval': '15second',
-                'span': 'hour',
-                'bounds': '24_7',
-                'index': 10
-            },
-            'trader': {
-                'interval': '15second',
-                'span': 'hour',
-                'bounds': '24_7'
-            },
-            'determine_trade_function': 'function_name',
-            'cash': 2000,
-            'use_cash': True,
-            'loss_threshold': 50.00,
-            'holdings_factor': 0.20,
-            'cash_factor': 0.20
-        }
+    'crypto': ['BTC', 'ETH'],
+    'username': 'your_robinhood_username',
+    'password': 'your_robinhood_password',
+    'days_to_run': 1,
+    'export_csv': False,
+    'plot_analytics': False,
+    'plot_crypto': False,
+    'plot_portfolio': False,
+    'mode': 'safelive',
+    'backtest': {
+        'interval': '15second',
+        'span': 'hour',
+        'bounds': '24_7',
+        'index': 10
+    },
+    'trader': {
+        'interval': '15second',
+        'span': 'hour',
+        'bounds': '24_7'
+    },
+    'determine_trade_function': 'personal_strategy',
+    'cash': 2000,
+    'use_cash': True,
+    'loss_threshold': 50.00,
+    'loss_percentage': 5,
+    'holdings_factor': 0.20,
+    'cash_factor': 0.20
+}
 
 tr = trader.Trader(config)
 
--- test.py
+++ test.py
@@ -13,34 +13,34 @@ def personal_strategy(crypto, times, prices):
 
 def test_trader():
     config = {
-                'crypto': ['BTC', 'ETH'],
-                'username': 'your_robinhood_username',
-                'password': 'your_robinhood_password',
-                'days_to_run': 1,
-                'export_csv': False,
-                'plot_analytics': False,
-                'plot_crypto': False,
-                'plot_portfolio': False,
-                'mode': 'safelive',
-                'backtest': {
-                    'interval': '15second',
-                    'span': 'hour',
-                    'bounds': '24_7',
-                    'index': 10
-                },
-                'trader': {
-                    'interval': '15second',
-                    'span': 'hour',
-                    'bounds': '24_7'
-                },
-                'determine_trade_function': 'personal_strategy',
-                'cash': 2000,
-                'use_cash': True,
-                'loss_threshold': 50.00,
-                'loss_percentage': 5,
-                'holdings_factor': 0.20,
-                'cash_factor': 0.20
-            }
+        'crypto': ['BTC', 'ETH'],
+        'username': 'your_robinhood_username',
+        'password': 'your_robinhood_password',
+        'days_to_run': 1,
+        'export_csv': False,
+        'plot_analytics': False,
+        'plot_crypto': False,
+        'plot_portfolio': False,
+        'mode': 'safelive',
+        'backtest': {
+            'interval': '15second',
+            'span': 'hour',
+            'bounds': '24_7',
+            'index': 10
+        },
+        'trader': {
+            'interval': '15second',
+            'span': 'hour',
+            'bounds': '24_7'
+        },
+        'determine_trade_function': 'personal_strategy',
+        'cash': 2000,
+        'use_cash': True,
+        'loss_threshold': 50.00,
+        'loss_percentage': 5,
+        'holdings_factor': 0.20,
+        'cash_factor': 0.20
+    }
 
     tr = trader.Trader(config)
 
