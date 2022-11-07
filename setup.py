6eb2e2c0f540a2217e668c8437f9311017db54ce
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2022-11-06 22:23:41 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="robinhood_crypto_trader",
     version="1.0.0",
     author="Tyler Pruitt",
     description="A Python package for an automated cryptocurrency trader on the popular trading platform Robinhood",
     url="https://github.com/tyler-pruitt/robinhood-crypto-trader",
-    packages=["robinhood_crypto_trader"],
-    requires=["numpy", "robin_stocks", "pandas", "datetime", "time", "matplotlib", "pandas_ta", "random", "sys", "discord_webhook"],
-    install_requires=["numpy", "robin_stocks", "pandas", "datetime", "time", "matplotlib", "pandas_ta", "random", "sys", "discord_webhook"],
+    packages=find_packages(),
+    requires=[
+        "numpy",
+        "robin_stocks",
+        "pandas",
+        "datetime",
+        "time",
+        "matplotlib",
+        "pandas_ta",
+        "random",
+        "sys",
+        "discord_webhook"
+    ],
+    install_requires=[
+        "numpy",
+        "robin_stocks",
+        "pandas",
+        "datetime",
+        "time",
+        "matplotlib",
+        "pandas_ta",
+        "random",
+        "sys",
+        "discord_webhook"
+    ],
     keywords=["robinhood", "crypto", "cryptocurrency", "trading", "autotrading", "investing", "robinhood crypto trader"],
     license="MIT"
 )
